1448e12e5f10f1b2d014a9d01e41f6286f9991ce
ZK <1099571219@qq.com>
2023-11-06 23:49:26 +0800

    fix: 注释

--- plugins/banner-plugin.js
+++ plugins/banner-plugin.js
@@ -12,22 +12,18 @@ module.exports = class BannerPlugin {
                 //1. 获取即将输出的资源: compilation.assets
                 //2. 过滤只保留 js 和 css 文件
                 //3. 遍历剩下资源添加注释
-                // console.log(compilation.assets);
                 const prefix =
                     `/*
                 * Author: ${this.options.author} 
                 */`
-                // const assets = compilation.getAssets()
-
-
                 Object.keys(assets).filter(key => {
                     const name = key.split('.')
                     return name.includes('js') || name.includes('css')
                 }).forEach(key => {
-                    const assetsRaw = assets
+                    //通过 source() 获取文件内容
                     const source = assets[key].source()
                     const content = prefix + source
-                    const com = compilation
+                    // 通过 RawSource 创建一个新的 Source 对象
                     assets[key] = new compilation.compiler.webpack.sources.RawSource(content);
                 })
                 callback()
